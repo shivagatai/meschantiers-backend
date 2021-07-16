aa21c7df12fe6c5407bd3220883f292f1a8dfcba
jd.bouvier <jean-damien.bouvier@calvados.fr>
2021-07-16 16:02:06 +0200

    Update database config

---
+++ config/env/production/database.js
@@ -0,0 +1,27 @@
+// from https://strapi.io/documentation/developer-docs/latest/setup-deployment-guides/deployment/hosting-guides/heroku.html
+
+const parse = require("pg-connection-string").parse;
+const config = parse(process.env.DATABASE_URL);
+
+module.exports = ({ env }) => ({
+  defaultConnection: "default",
+  connections: {
+    default: {
+      connector: "bookshelf",
+      settings: {
+        client: "postgres",
+        host: config.host,
+        port: config.port,
+        database: config.database,
+        username: config.user,
+        password: config.password,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      },
+      options: {
+        ssl: true,
+      },
+    },
+  },
+});
